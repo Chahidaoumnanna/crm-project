264d1b781009e7f8aba82e4b9e49dbf77df5dfe2
ouissal-yahyouni <ouissalyahyouni73@gmail.com>
2025-03-05 21:03:14 +0100

    pdf controller

--- app/Http/Controllers/pdfController.php
+++ app/Http/Controllers/pdfController.php
@@ -40,10 +40,10 @@ class PdfController extends Controller
         // Récupérer le bon de livraison avec ses items, client et paiements
         $bon = BonLivraison::with('items', 'client', 'paiements')->findOrFail($id);
 
-        // Charger la vue pdf bon pour générer le PDF
+        // Charger la vue pdf.bon pour générer le PDF
         $pdf = Pdf::loadView('pdf.bon', compact('bon'));
 
         // Retourner le PDF
         return $pdf->stream(); // Cela permettra de l'afficher directement dans le navigateur
-        }
+    }
 }
